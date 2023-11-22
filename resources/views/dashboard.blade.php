f1a20545f5f3f521d0e1f8a37ce10b1a015f99d7
mkltyg <mikaelapring1422@gmail.com>
2023-11-23 00:30:37 +0800

    fifth commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,7 @@
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     </head>
 <body>
+    <div class="profile"></div>
     @include('components/navbar')
     <header>
         <nav>
